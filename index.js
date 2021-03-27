4129d5603e1fe9a486e533e6caaba00ba1ee6dfa
MatheusEli <matheuselisouzaferreira@hotmail.com>
2021-03-27 00:52:53 -0300

    mudando identity

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const twilioApiSecret = 'KOUAOQ63Nm8V7UlT7bVCvseAcMDB5PmO';
 // Create an access token which we will sign and return to the client,
 // containing the grant we just created
 const token = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret);
-token.identity = 'Matheus_eli';
+token.identity = 'Convidado';
 
 // Create a Video grant which enables a client to use Video 
 // and limits access to the specified Room (DailyStandup)
