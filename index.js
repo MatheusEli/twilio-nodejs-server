681efac7bd8b57794df0a53509a9430c4cb84b1e
MatheusEli <matheuselisouzaferreira@hotmail.com>
2021-03-27 20:57:15 -0300

    .

--- index.js
+++ index.js
@@ -5,22 +5,25 @@ const app = express();
 const port = process.env.PORT || 4000 ;
 app.listen(port, () => console.log("servidor rodando na porta: "+port+"com sucesso!"));
 
-const VideoGrant = AccessToken.VideoGrant;
-
-// Used when generating any kind of Access Token
-const twilioAccountSid = 'ACb37498f7c3cfb048887cc7f437d22050';
-const twilioApiKey = 'SK20f33b227c80b3e35ec3130b53072223';
-const twilioApiSecret = 'KOUAOQ63Nm8V7UlT7bVCvseAcMDB5PmO';
-
-// Create an access token which we will sign and return to the client,
-// containing the grant we just created
-const token = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret);
 
 
 app.get('/token/:room/:identity', (req, res) => {
     
+    const VideoGrant = AccessToken.VideoGrant;
+    
+    // Used when generating any kind of Access Token
+    const twilioAccountSid = 'ACb37498f7c3cfb048887cc7f437d22050';
+    const twilioApiKey = 'SK20f33b227c80b3e35ec3130b53072223';
+    const twilioApiSecret = 'KOUAOQ63Nm8V7UlT7bVCvseAcMDB5PmO';
+    
+    // Create an access token which we will sign and return to the client,
+    // containing the grant we just created
+    const token = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret);
+    
     const room = req.params.room;
     const identity = req.params.identity;
+
+    console.log('Room: '+room+' | Identity: '+identity);
     
     
     token.identity = identity;
