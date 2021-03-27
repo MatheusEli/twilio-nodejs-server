3404472f51aa34dd13f5bf939dbf963549f8ddc7
MatheusEli <matheuselisouzaferreira@hotmail.com>
2021-03-27 15:12:19 -0300

    configurando get para recebimento de dados

--- index.js
+++ index.js
@@ -15,21 +15,27 @@ const twilioApiSecret = 'KOUAOQ63Nm8V7UlT7bVCvseAcMDB5PmO';
 // Create an access token which we will sign and return to the client,
 // containing the grant we just created
 const token = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret);
-token.identity = 'Matheus_eli';
-
-// Create a Video grant which enables a client to use Video 
-// and limits access to the specified Room (DailyStandup)
-const videoGrant = new VideoGrant({
-    room: 'hello_world_2'
-});
-
-// Add the grant to the token
-token.addGrant(videoGrant);
-
-// Serialize the token to a JWT string
-console.log(token.toJwt());
 
 
-app.get('/token', (req, res) => {
+app.get('/token/:room/:identity', (req, res) => {
+    
+    const room = req.params.room;
+    const identity = req.params.identity;
+    
+    
+    token.identity = identity;
+    
+    // Create a Video grant which enables a client to use Video 
+    // and limits access to the specified Room (DailyStandup)
+    const videoGrant = new VideoGrant({
+        room: room
+    });
+    
+    // Add the grant to the token
+    token.addGrant(videoGrant);
+    
+    // Serialize the token to a JWT string
+    console.log(token.toJwt());
+
     res.send({ token: token.toJwt() });
 });
