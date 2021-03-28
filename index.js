5a46a7d45151e8e0ef13f3b04feec996a1a3614e
MatheusEli <matheuselisouzaferreira@hotmail.com>
2021-03-27 21:57:06 -0300

    configurando identity

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/token/:room/:identity', (req, res) => {
     // Create an access token which we will sign and return to the client,
     // containing the grant we just created
     const token = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret);
-    token.identity = 'Matheus eli';
+    token.identity = ''+req.params.identity;
     const room = req.params.room;
     
     console.log(token.identity);
